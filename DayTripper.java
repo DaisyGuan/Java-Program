b37b6f0426e53aeb3eefe325fba102b623461647
DaisyGuan <yguan11@jhu.edu>
2017-01-07 11:16:29 -0500

    Create DayTripper.java

---
+++ DayTripper.java
@@ -0,0 +1,16 @@
+
+public class DayTripper {
+	public enum Day{MON,TUE,WED,THU,FRI,SAT,SUN};
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Day d = Day.MON;
+		System.out.println("Initially d is "+ d);
+		d = Day.WED;
+		System.out.println("Then it is "+ d);
+		Day t = Day.valueOf("WED");
+		System.out.println("I say d and t are the same: "+ (d==t));
+
+	}
+
+}
